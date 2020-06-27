1816fd51536e2111a46e358f70316b4b25127d89
raya-coder <raya.lasmar@ensi-uma.tn>
2020-06-27 12:04:17 +0100

    Initial commit

---
+++ app/src/main/java/com/example/app1/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.app1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edID;
+    Button btnSubmit;
+    TextView tvResults;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);//in the 'R'esoures fold where the layout exist you get main_activity
+        edID = findViewById(R.id.edID);
+        btnSubmit = findViewById(R.id.btnSubmit);
+        tvResults = findViewById(R.id.tvResults);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String idNumber = edID.getText().toString().trim();
+            }
+        });
+    }
+}
